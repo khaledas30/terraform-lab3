7f9af17098eacd1dab074d159674e5248443849e
khaledas30 <ka3641694@gmail.com>
2023-01-19 02:35:50 +0200

    terraform

---
+++ loadbalancer/variables_lb.tf
@@ -0,0 +1,21 @@
+variable "security_group" {
+  type = list
+}
+variable "sub_alb_id" {
+  type = list
+}
+variable "vpc_tg_id" {
+  
+}
+
+variable "alb_type" {
+
+}
+variable "alb_name" {
+
+}
+variable "tg-alb_name" {
+
+}
+
+
